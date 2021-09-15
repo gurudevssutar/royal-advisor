2ed57a988354a0b9baeee2dfc292d22a1f53a2d5
gurudevssutar <gurudevssutar@gmail.com>
2021-09-15 21:18:55 +0530

    Drag and drop feature of tasks added

---
+++ lib/model/draggable_list.dart
@@ -0,0 +1,22 @@
+class DraggableList {
+  final String header;
+  final List<DraggableListItem> items;
+
+  const DraggableList({
+    required this.header,
+    required this.items,
+  });
+}
+
+class DraggableListItem {
+  final String title;
+  final String urlImage;
+  final int arrivalTime;
+  final int executeTime;
+
+  const DraggableListItem(
+      {required this.title,
+      required this.urlImage,
+      required this.arrivalTime,
+      required this.executeTime});
+}
