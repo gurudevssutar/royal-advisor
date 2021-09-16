70380cd53284fd0822b5afab141edfb4d027a679
Priyang <p.c90909@gmail.com>
2021-09-16 14:37:11 +0530

    Rive Testing- Maja aa gaya

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
 
 import 'model/draggable_list.dart';
 
@@ -51,8 +52,8 @@ class _MainPage extends State<MainPage> {
   bool anscheck(List<DraggableList> soln, List<String> ans) {
     // Code to check answer
     int j = 0;
-    for(DraggableListItem i in soln[0].items){
-      if(i.id != ans[j]){
+    for (DraggableListItem i in soln[0].items) {
+      if (i.id != ans[j]) {
         return false;
       }
       j++;
@@ -96,11 +97,26 @@ class _MainPage extends State<MainPage> {
               onListReorder: onReorderList,
             ),
           ),
+          Container(
+              width: 200,
+              height: 200,
+              child: RiveAnimation.asset('assets/checkmark_icon.riv')),
+          ElevatedButton(
+            child: const Text('Play One-Shot Animation'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => const PlayOneShotAnimation(),
+                ),
+              );
+            },
+          ),
           ElevatedButton(
             onPressed: () {
-              if(anscheck(current, anslist)){
+              if (anscheck(current, anslist)) {
                 print('Answer is correct');
-              }else{
+              } else {
                 print('Invalid Answer');
               }
             },
@@ -170,7 +186,7 @@ class _MainPage extends State<MainPage> {
       final oldListItems = lists[oldListIndex].children;
       final newListItems = lists[newListIndex].children;
 
-      for(DraggableListItem i in current[0].items){
+      for (DraggableListItem i in current[0].items) {
         print("${i.id}");
       }
       print("${oldItemIndex} ${oldListIndex} ${newItemIndex} ${newListIndex}");
@@ -181,7 +197,7 @@ class _MainPage extends State<MainPage> {
         newListItems.insert(newItemIndex, movedItem);
 
         current[0].items.insert(newItemIndex, movedTile);
-        for(DraggableListItem i in current[0].items){
+        for (DraggableListItem i in current[0].items) {
           print("${i.id}");
         }
       }
@@ -196,5 +212,54 @@ class _MainPage extends State<MainPage> {
       final movedList = lists.removeAt(oldListIndex);
       lists.insert(newListIndex, movedList);
     });
+  }
+}
+
+
+class PlayOneShotAnimation extends StatefulWidget {
+  const PlayOneShotAnimation({Key? key}) : super(key: key);
+
+  @override
+  _PlayOneShotAnimationState createState() => _PlayOneShotAnimationState();
+}
+
+class _PlayOneShotAnimationState extends State<PlayOneShotAnimation> {
+  /// Controller for playback
+  late RiveAnimationController _controller;
+
+  /// Is the animation currently playing?
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = OneShotAnimation(
+      'bounce',
+      autoplay: false,
+      onStop: () => setState(() => _isPlaying = false),
+      onStart: () => setState(() => _isPlaying = true),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('One-Shot Example'),
+      ),
+      body: Center(
+        child: RiveAnimation.network(
+          'https://cdn.rive.app/animations/vehicles.riv',
+          animations: const ['idle', 'curves'],
+          controllers: [_controller],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // disable the button while playing the animation
+        onPressed: () => _isPlaying ? null : _controller.isActive = true,
+        tooltip: 'Play',
+        child: const Icon(Icons.arrow_upward),
+      ),
+    );
   }
 }
